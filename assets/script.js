04581565a505f396f9db9e7a7cdea80addcceb8d
Hakki <mohdhakki12@hotmail.com>
2021-11-19 17:16:04 +0200

    FEAT : Added an event listener for the save button on the each time block

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,12 @@
 var time = moment().format("dddd MMMM Do");
 $("#currentDay").text(time);
+
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+  });
+
+  
