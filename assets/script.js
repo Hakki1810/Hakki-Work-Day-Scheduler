7f223fdbbd860207dbc1f89fc9bf6e7fc1fc6c88
Hakki <mohdhakki12@hotmail.com>
2021-11-21 17:18:37 +0200

    Added the id's in the script.js file for all the timings in to be saved to the local storage

--- assets/script.js
+++ assets/script.js
@@ -29,4 +29,20 @@ $(document).ready(function () {
       }
     });
   }
+  $("#hour8 .description").val(localStorage.getItem("hour8"));
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
+  $("#hour18 .description").val(localStorage.getItem("hour18"));
+  $("#hour19 .description").val(localStorage.getItem("hour19"));
+  $("#hour20 .description").val(localStorage.getItem("hour20"));
+  $("#hour21 .description").val(localStorage.getItem("hour21"));
+
+  timeTracker();
 });
